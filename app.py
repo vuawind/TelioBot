0c6b63df84fa27b37e6518bc64716f0712c082e0
Phong Mai <57877225+vuawind@users.noreply.github.com>
2021-06-21 03:23:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 
 import os
 from time import time
-from datetime import datetime
+import datetime
 from slack_bolt import App, Ack
 from slack import WebClient
 from travel import travel_bot
-
+from hrwelcome import welcome
+from hranswer import *
 
 app = App(
     signing_secret = os.environ.get("SLACK_EVENTS_TOKEN"),
@@ -13,6 +14,11 @@ app = App(
 # Initialize a Web API client
     token=os.environ.get("SLACK_TOKEN")
 )
+
+log_channel='C0252EMRTS7'
+admin_channel='C024Z67LNDB'
+bot_channel='D024MFB2B1D'
+hr_channel='C025751TS0P'
 
 @app.shortcut("ice")
 def travel_modal(shortcut, say, client, ack):
@@ -46,12 +52,12 @@ def handle_submission(ack, body, client, view, logger, message, user):
     # Validate the inputs
     # Acknowledge the view_submission event and close the modal
     errors = {}
-    date1 = datetime.strptime(line1, "%Y-%m-%d")
-    date2 = datetime.strptime(line3, "%Y-%m-%d")
-    date3 = datetime.strptime(datetime.today().strftime('%Y-%m-%d'), "%Y-%m-%d")
-    timestamp1 = datetime.timestamp(date1)
-    timestamp2 = datetime.timestamp(date2)
-    timestamp3 = datetime.timestamp(date3)
+    date1 = datetime.datetime.strptime(line1, "%Y-%m-%d")
+    date2 = datetime.datetime.strptime(line3, "%Y-%m-%d")
+    date3 = datetime.datetime.strptime(datetime.datetime.today().strftime('%Y-%m-%d'), "%Y-%m-%d")
+    timestamp1 = datetime.datetime.timestamp(date1)
+    timestamp2 = datetime.datetime.timestamp(date2)
+    timestamp3 = datetime.datetime.timestamp(date3)
     if timestamp1 is not None and timestamp1 > timestamp2:
         errors["block_a"] = "Departure date cannot be later than return date"
     if timestamp1 - timestamp3 < 259200:
@@ -74,7 +80,7 @@ def handle_submission(ack, body, client, view, logger, message, user):
 
     # Message the user
     try:
-        client.chat_postMessage(channel='C0252EMRTS7', blocks=[
+        client.chat_postMessage(channel=log_channel, blocks=[
 		{
 			"type": "section",
 			"text": {
@@ -95,7 +101,7 @@ def handle_submission(ack, body, client, view, logger, message, user):
 			}
 		}
 	])
-        client.chat_postMessage(channel=f'{line11}', blocks=[
+        client.chat_postMessage(channel=line11, blocks=[
 		{
 			"type": "section",
 			"text": {
@@ -151,28 +157,37 @@ def handle_submission(ack, body, client, view, logger, message, user):
     except e:
         logger.exception(f"Failed to post a message {e}")
 
-@app.view("view_travel_update")
-def view_update(ack, body, client, view, logger, message, user, say):
-    ack()
-    say("nice")
-
 @app.action("button1")
 def action_button_click1(body, ack, say, client, view):
     # Acknowledge the action
-    user = body["user"]["id"]
     ts=body['message']['ts']
-    result = client.conversations_history(channel = 'C0252EMRTS7', inclusive=True,latest=ts,limit=1)
+    result = client.conversations_history(channel = log_channel, inclusive=True,latest=ts,limit=1)
     conversation_history = f"{result['messages'][0]['blocks'][0]['text']['text']}\n{result['messages'][0]['blocks'][1]['text']['text']}"
     msg = conversation_history
     ack()
-    client.chat_postMessage(channel = body['message']['blocks'][0]['text']['text'], text = f"<@{body['user']['id']}> approved your travel plan")
-    client.chat_postMessage(channel = user, text = f"<@{body['user']['id']}> approved")
-    client.chat_postMessage(channel = 'C024Z67LNDB', blocks=[
+    client.chat_postMessage(channel = body['message']['blocks'][0]['text']['text'], text = f"<@{body['user']['id']}> approved")
+    client.chat_update(ts=ts,channel = bot_channel, blocks=[
 		{
 			"type": "section",
 			"text": {
 				"type": "mrkdwn",
-				"text": f"You have a new request from *<@{body['user']['username']}>*"
+				"text": msg
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": f":white_check_mark: You have approved this request"
+			}
+		}
+	])
+    client.chat_postMessage(channel = admin_channel, blocks=[
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": f"Line manager *<@{body['user']['username']}>* approved this travel plan"
 			}
 		},
 		{
@@ -202,16 +217,16 @@ def action_button_click1(body, ack, say, client, view):
 @app.action("button_done")
 def info_click(body, say, client, ack, action):
     ts=body['message']['ts']
-    result = client.conversations_history(channel = "C024Z67LNDB", inclusive=True,latest=ts,limit=1)
+    result = client.conversations_history(channel = admin_channel, inclusive=True,latest=ts,limit=1)
     conversation_history = f"{result['messages'][0]['blocks'][0]['text']['text']}\n{result['messages'][0]['blocks'][1]['text']['text']}"
     msg = conversation_history
     ack()
-    client.chat_update(ts=ts, channel = 'C024Z67LNDB', blocks=[
+    client.chat_update(ts=ts, channel = admin_channel, blocks=[
 		{
 			"type": "section",
 			"text": {
 				"type": "mrkdwn",
-				"text": f"You have a new request from *<@{body['user']['username']}>*"
+				"text": f"You have finished booking for this travel plan"
 			}
 		},
 		{
@@ -249,8 +264,8 @@ def handle_info_travel(ack, body, client, view, logger, message, user,say):
     line7 = view["state"]["values"]["block_g_input"]["hotel_location"]["value"]
     line8 = view["state"]["values"]["block_h_input"]["hotel_num"]["value"]
     line9 = view["state"]["values"]["block_j_input"]["boarding_pass"]["value"]
-    line10 = view["state"]["values"]["block_k_input"]["note_id"]["value"]
     line11 = view["state"]["values"]["block_l_input"]["flight_ticket"]["value"]
+    line10 = view["state"]["values"]["block_k_input"]["note_id"]["value"]
     response = client.users_info(user=line1)
     assert(response)
     profile = response['user']['profile']['display_name']
@@ -295,9 +310,229 @@ def handle_info_travel(ack, body, client, view, logger, message, user,say):
 @app.action("button2")
 def action_button_click2(body, ack, say, client):
     # Acknowledge the action
+    ts=body['message']['ts']
+    result = client.conversations_history(channel = log_channel, inclusive=True,latest=ts,limit=1)
+    conversation_history = f"{result['messages'][0]['blocks'][0]['text']['text']}\n{result['messages'][0]['blocks'][1]['text']['text']}"
+    msg = conversation_history
     ack()
-    client.chat_postMessage(channel = body['message']['blocks'][0]['text']['text'], text = f"<@{body['user']['id']}> denied your travel plan")
+    client.chat_postMessage(as_user=True,channel = body['message']['blocks'][0]['text']['text'], text = f"<@{body['user']['id']}> denied your travel plan")
+    client.chat_update(ts=ts, channel = bot_channel, blocks=[
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": msg
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": f":negative_squared_cross_mark: You have denied this request"
+			}
+		}
+	])
 
+@app.message("print")
+def new_comer(body, ack, say, client):
+    ack()
+    client.chat_postMessage(channel=log_channel, blocks=welcome.WELCOME_BLOCK)
+    #response = client.team_accessLogs(team_id=)
+
+@app.event("team_join")
+def ask_for_introduction(event, say, client, body):
+    tomorrow = datetime.datetime.today() + datetime.timedelta(days=7)
+    scheduled_time = datetime.time(hour=9, minute=30, second=10)
+    schedule_timestamp = datetime.datetime.combine(tomorrow, scheduled_time).strftime('%s')
+    user_id = event["user"]['id']
+    text2 = f"🎉 Xin chào <@{user_id}>, Bạn có câu hỏi về chế độ, chính sách, thủ tục nhân sự? Hãy hỏi với Sophie bằng cách bấm vào mục liên quan bên dưới nhé!"
+    say(text=text2, channel=user_id)
+    client.chat_postMessage(channel=user_id, blocks=welcome.WELCOME_BLOCK)
+    client.chat_scheduleMessage(channel=user_id,text=f"Hello <@{body['user']['id']}>", blocks=welcome.WELCOME_BLOCK, post_at=schedule_timestamp)
+
+@app.shortcut("sophie")
+def sophie(event, say, ack, client,shortcut, body):
+    ack()
+    client.chat_postMessage(channel=shortcut['user']['id'], text=f"Xin chào <@{body['user']['id']}>, Tôi là Sophie - HR Chatbot. Bạn có câu hỏi nào cho Sophie - Hãy bấm vào mục liên quan bên dưới nhé!")
+    client.chat_postMessage(channel=shortcut['user']['id'], blocks=welcome.WELCOME_BLOCK)
+
+@app.action("onboard_id")
+def onboard(event, say, ack, client,body):
+    ack()
+    client.chat_postMessage(channel=body['user']['id'], blocks=welcome.BLOCK_ONBOARD)
+
+@app.action("quitjob_id")
+def quit(event, say, ack, client,body):
+    ack()
+    client.chat_postMessage(channel=body['user']['id'], blocks=welcome.BLOCK_QUIT)
+    
+@app.action("move_id")
+def move(event, say, ack, client,body):
+    ack()
+    client.chat_postMessage(channel=body['user']['id'], blocks=welcome.BLOCK_MOVE)
+
+@app.action("insurance_id")
+def insurance(event, say, ack, client,body):
+    ack()
+    client.chat_postMessage(channel=body['user']['id'], blocks=welcome.BLOCK_INSURANCE)
+
+@app.action("tax_id")
+def tax(event, say, ack, client,body):
+    ack()
+    client.chat_postMessage(channel=body['user']['id'], blocks=welcome.BLOCK_TAX)
+
+@app.action("salary_id")
+def salary(event, say, ack, client,body):
+    ack()
+    client.chat_postMessage(channel=body['user']['id'], blocks=welcome.BLOCK_SALARY)
+
+@app.action("work_id")
+def work(event, say, ack, client,body):
+    ack()
+    client.chat_postMessage(channel=body['user']['id'], blocks=welcome.BLOCK_WORK)
+
+@app.action("other_id")
+def other(event, say, ack, client,body):
+    ack()
+    client.chat_postMessage(channel=body['user']['id'], blocks=welcome.BLOCK_OTHER)
+
+@app.action("onboard_select")
+def onboard_choice(event, say, ack, client,body,action):
+    selected_option = body['state']['values']['block_a']['onboard_select']['selected_option']['value']
+    ack()
+    if selected_option == 'value-0':
+        client.chat_postMessage(channel=body['user']['id'], text=onboard_answer.ans1)
+    if selected_option == 'value-1':
+        client.chat_postMessage(channel=body['user']['id'], text=onboard_answer.ans2)
+    if selected_option == 'value-2':
+        client.chat_postMessage(channel=body['user']['id'], text=onboard_answer.ans3)
+    if selected_option == 'value-3':
+        client.chat_postMessage(channel=body['user']['id'], text=onboard_answer.ans4)
+    if selected_option == 'value-4':
+        client.chat_postMessage(channel=body['user']['id'], text=onboard_answer.ans5)
+    if selected_option == 'value-5':
+        client.chat_postMessage(channel=body['user']['id'], text=onboard_answer.ans6)
+    if selected_option == 'value-6':
+        client.chat_postMessage(channel=body['user']['id'], text=onboard_answer.ans7)
+    if selected_option == 'value-7':
+        client.chat_postMessage(channel=body['user']['id'], text=onboard_answer.ans8)
+
+@app.action("quit_select")
+def quit_choice(event, say, ack, client,body,action):
+    selected_option = body['state']['values']['block_b']['quit_select']['selected_option']['value']
+    ack()
+    if selected_option == 'value-0':
+        client.chat_postMessage(channel=body['user']['id'], text=quit_answer.ans1)
+    if selected_option == 'value-1':
+        client.chat_postMessage(channel=body['user']['id'], text=quit_answer.ans2)
+    if selected_option == 'value-2':
+        client.chat_postMessage(channel=body['user']['id'], text=quit_answer.ans3)
+    if selected_option == 'value-3':
+        client.chat_postMessage(channel=body['user']['id'], text=quit_answer.ans4)
+    if selected_option == 'value-4':
+        client.chat_postMessage(channel=body['user']['id'], text=quit_answer.ans5)
+
+@app.action("move_select")
+def move_choice(event, say, ack, client,body,action):
+    selected_option = body['state']['values']['block_c']['move_select']['selected_option']['value']
+    ack()
+    if selected_option == 'value-0':
+        client.chat_postMessage(channel=body['user']['id'], text=move_answer.ans1)
+    if selected_option == 'value-1':
+        client.chat_postMessage(channel=body['user']['id'], text=move_answer.ans2)
+    if selected_option == 'value-2':
+        client.chat_postMessage(channel=body['user']['id'], text=move_answer.ans3)
+
+@app.action("insurance_select")
+def insurance_choice(event, say, ack, client,body,action):
+    selected_option = body['state']['values']['block_d']['insurance_select']['selected_option']['value']
+    ack()
+    if selected_option == 'value-0':
+        client.chat_postMessage(channel=body['user']['id'], text=insurance_answer.ans1)
+    if selected_option == 'value-1':
+        client.chat_postMessage(channel=body['user']['id'], text=insurance_answer.ans2)
+    if selected_option == 'value-2':
+        client.chat_postMessage(channel=body['user']['id'], text=insurance_answer.ans3)
+    if selected_option == 'value-3':
+        client.chat_postMessage(channel=body['user']['id'], text=insurance_answer.ans4)
+    if selected_option == 'value-4':
+        client.chat_postMessage(channel=body['user']['id'], text=insurance_answer.ans5)
+    if selected_option == 'value-5':
+        client.chat_postMessage(channel=body['user']['id'], text=insurance_answer.ans6)
+    if selected_option == 'value-6':
+        client.chat_postMessage(channel=body['user']['id'], text=insurance_answer.ans7)
+    if selected_option == 'value-7':
+        client.chat_postMessage(channel=body['user']['id'], text=insurance_answer.ans8)
+    if selected_option == 'value-8':
+        client.chat_postMessage(channel=body['user']['id'], text=insurance_answer.ans9)
+
+@app.action("tax_select")
+def tax_choice(event, say, ack, client,body,action):
+    selected_option = body['state']['values']['block_e']['tax_select']['selected_option']['value']
+    ack()
+    if selected_option == 'value-0':
+        client.chat_postMessage(channel=body['user']['id'], text=tax_answer.ans1)
+    if selected_option == 'value-1':
+        client.chat_postMessage(channel=body['user']['id'], text=tax_answer.ans2)
+    if selected_option == 'value-2':
+        client.chat_postMessage(channel=body['user']['id'], text=tax_answer.ans3)
+    if selected_option == 'value-3':
+        client.chat_postMessage(channel=body['user']['id'], text=tax_answer.ans4)
+    if selected_option == 'value-4':
+        client.chat_postMessage(channel=body['user']['id'], text=tax_answer.ans5)
+    if selected_option == 'value-5':
+        client.chat_postMessage(channel=body['user']['id'], text=tax_answer.ans6)
+
+@app.action("salary_select")
+def salary_choice(event, say, ack, client,body,action):
+    selected_option = body['state']['values']['block_f']['salary_select']['selected_option']['value']
+    ack()
+    if selected_option == 'value-0':
+        client.chat_postMessage(channel=body['user']['id'], text=salary_answer.ans1)
+    if selected_option == 'value-1':
+        client.chat_postMessage(channel=body['user']['id'], text=salary_answer.ans2)
+    if selected_option == 'value-2':
+        client.chat_postMessage(channel=body['user']['id'], text=salary_answer.ans3)
+    if selected_option == 'value-3':
+        client.chat_postMessage(channel=body['user']['id'], text=salary_answer.ans4)
+    if selected_option == 'value-4':
+        client.chat_postMessage(channel=body['user']['id'], text=salary_answer.ans5)
+    if selected_option == 'value-5':
+        client.chat_postMessage(channel=body['user']['id'], text=salary_answer.ans6)
+    if selected_option == 'value-6':
+        client.chat_postMessage(channel=body['user']['id'], text=salary_answer.ans7)
+    if selected_option == 'value-7':
+        client.chat_postMessage(channel=body['user']['id'], text=salary_answer.ans8)
+    if selected_option == 'value-8':
+        client.chat_postMessage(channel=body['user']['id'], text=salary_answer.ans9)
+    if selected_option == 'value-9':
+        client.chat_postMessage(channel=body['user']['id'], text=salary_answer.ans10)
+    if selected_option == 'value-10':
+        client.chat_postMessage(channel=body['user']['id'], text=salary_answer.ans11)
+    if selected_option == 'value-11':
+        client.chat_postMessage(channel=body['user']['id'], text=salary_answer.ans12)
+
+@app.action("work_select")
+def work_choice(event, say, ack, client,body,action):
+    selected_option = body['state']['values']['block_g']['work_select']['selected_option']['value']
+    ack()
+    if selected_option == 'value-0':
+        client.chat_postMessage(channel=body['user']['id'], text=work_answer.ans1)
+    if selected_option == 'value-1':
+        client.chat_postMessage(channel=body['user']['id'], text=work_answer.ans2)
+    if selected_option == 'value-2':
+        client.chat_postMessage(channel=body['user']['id'], text=work_answer.ans3)
+
+@app.action("submit")
+def other_text(event, say, ack, client,body,action,button):
+    text = body['state']['values']['block_h']['other_input']['value']
+    ack()
+    client.chat_postMessage(channel = hr_channel, text = f"you have a new question from <@{body['user']['id']}>:\n{text}")
+
+@app.action("back")
+def back(event, say, ack, client,body,action):
+    ack()
+    client.chat_postMessage(channel = body['user']['id'], blocks=welcome.WELCOME_BLOCK)
 
 # Start your app
 if __name__ == "__main__":
